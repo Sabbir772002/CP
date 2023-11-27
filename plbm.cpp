112aef8c3b761e8f1a71a308057040f4240d60c9
sabbir772002 <sabbir772002@gmail.com>
2023-11-27 18:54:32 +0600

    S

---
+++ plbm.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define endl               "\n"
+#define fast               ios::sync_with_stdio(0);cin.tie(0);
+#define ll                 long long
+#define fr(i, n)           for (ll i=0;i<n;i++)
+#define fr1(i, n)          for(ll i=1;i<=n;i++)
+#define tc                 int t; cin >> t;   while(t--)
+#define pb                 push_back
+#define fs(n)              fixed<<setprecision(n)
+#define asort(a)           sort(a,a+n)
+#define vi                 vector<int>
+#define qi                 queue<int>
+#define di                 deque<int>
+#define pqi                priority_queue<int>
+#define pql                priority_queue<long long>
+#define dl                 deque<long long>
+#define ql                 queue<long long>
+#define s                  second
+#define f                  first
+#define vl                 vector<long long int>
+#define vc                 vector<char>
+#define po                 pop_back
+#define in(v)               for(auto &b:v)cin>>b;
+#define allr(x)            (x).rbegin(), (x).rend()
+#define all(x)             (x).begin(), (x).end()
+#define dsort(a)           sort(a,a+n,greater<int>())
+#define tolower(x)         transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define mod                1000000007
+#define mx                 10000005
+
+
+
+int main() {
+  
+     #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin);
+       freopen("output.txt", "w", stdout);
+     #endif
+
+
+tc{
+
+}
+}
